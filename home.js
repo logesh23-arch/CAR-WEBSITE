13ad3eb22fb418da963f454342e6301fd4222a2b
logeeshwara <palogeeshwara23@gmail.com>
2024-12-18 19:35:16 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,44 @@
+var container = document.getElementById("search-box")
+var input = document.getElementById("sea-inp")
+var navbar = document.getElementById("search-navbar")
+
+navbar.addEventListener("click",function(){
+    
+    container.style.display="inline-block"
+    input.style.display="inline-block"
+})
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+  $(document).ready(function(){
+    $(".preloader").fadeOut();
+    })
+
+
+
